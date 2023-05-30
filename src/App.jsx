d3c1dcb64b7f7ae399c8e76b1e1b41592b9a24f7
sudonomen <sudonomen@proton.me>
2023-05-29 20:50:31 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(false)
 
   return (
-    <div className='app'>
+    <>
+    <CSSTransition timeout={1000} in={count} classNames="fade">
+    <div className='app' >
       Hello World
     </div>
+    </CSSTransition>
+        <button onClick={()=>{setCount(!count)}}>Click</button>
+        </>
   )
 }
 
