23a81f0644ec08939dcf52a27f9b5fd60f4c44ce
sudonomen <sudonomen@proton.me>
2023-05-31 00:00:15 -0500

    testing new html 3

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,17 @@ function App() {
 
   return (
     <>
+    <label for="pet-select">Choose a pet:</label>
+
+<select name="pets" id="pet-select">
+    <option value="">--Please choose an option--</option>
+    <option value="dog">Dog</option>
+    <option value="cat">Cat</option>
+    <option value="hamster">Hamster</option>
+    <option value="parrot">Parrot</option>
+    <option value="spider">Spider</option>
+    <option value="goldfish">Goldfish</option>
+</select>
     <input contentEditable={false} type="number" list='nl'/>
     <datalist id='nl'>
       <option value='1'></option>
